48b554713925287530abe0d7786ebf8a24ae137c
S@twik <satwiksinha1011@gmail.com>
2026-08-26 11:18:06 +0530

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Transfinitte-24
+
+## Tech-Stack
+- React+Tailwind CSS
---
+++ src/assets/index.js
@@ -0,0 +1,77 @@
+import numbersvg from "./svg/24.svg";
+import tfhero from "./svg/tf-hero.svg"
+import tfherosm from "./svg/tf-text-ph.svg"
+import bubble from "./svg/bubble.svg"
+import tfgridsm from "./svg/tf-grid-sm.svg"
+import aboutus from "./images/aboutus.jpeg"
+import anand from "./images/Anand Kumar.jpg"
+import anirudh from "./images/anirudh.jpg"
+import ayush from "./images/ayush.jpeg"
+import ram from "./images/ram.jpeg"
+import kumar from "./images/kumar.jpg"
+import subash from "./images/subash.jpg"
+import rakesh from "./images/rakesh.jpeg"
+import adwaith from "./images/adwaith.jpeg"
+import mudit from "./images/mudit.jpeg"
+
+
+import geeks from "./svg/gfg.svg"
+import netcon from "./svg/netcon.svg"
+import boeing from "./svg/boeing.svg"
+import tezos from "./svg/tezos.svg"
+import crewsphere from "./svg/crewsphere.svg"
+import bharatX from "./svg/bharatX.svg"
+import pathway from "./svg/pathway.svg"
+
+import groups from "./svg/groups.svg"
+import prizes from "./svg/prizes.svg"
+import ps from "./svg/ps.svg"
+import arrowwh from './svg/arrowwhite.svg'
+import arrowbl from './svg/arrowblack.svg'
+import admin from './svg/adminblock.svg'
+import tffooter from './svg/tf-footer.svg'
+import tffootersm from './svg/tf-footer-sm.svg'
+import tfbadge from './svg/tf-badge.svg'
+import instagram from './svg/instagram.svg'
+import instagramwh from './svg/instagramwh.svg'
+import linkedin from './svg/linkedin.svg'
+import linkedinwh from './svg/linkedinwh.svg'
+
+
+export {
+    numbersvg,
+    tfhero,
+    tfherosm,
+    bubble,
+    tfgridsm,
+    aboutus,
+    anand,
+    anirudh,
+    ayush,
+    ram,
+    kumar,
+    rakesh,
+    adwaith,
+    subash,
+    mudit,
+    groups,
+    prizes,
+    ps,
+    arrowwh,
+    arrowbl,
+    admin,
+    geeks,
+    netcon,
+    boeing,
+    tezos,
+    bharatX,
+    crewsphere,
+    pathway,
+    tffooter,
+    tffootersm,
+    instagram,
+    instagramwh,
+    linkedin,
+    linkedinwh,
+    tfbadge,
+    };
