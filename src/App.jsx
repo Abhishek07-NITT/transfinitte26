0f606c00aa9c7fd50d4ec5d50177e7b22c82835c
S@twik <satwiksinha1011@gmail.com>
2026-09-13 21:25:52 +0530

    remove judges section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const Timer = lazy(() => import("./components/Timer"));
 const AboutUs = lazy(() => import("./components/AboutUs"));
 const Domains = lazy(() => import("./components/DomainsPrize"));
 const Timeline = lazy(() => import("./components/Timeline"));
-const Judges = lazy(() => import("./components/Judges"));
+
 const PrizePool = lazy(() => import("./components/PrizePool"));
 const Sponsors = lazy(() => import("./components/Sponsors"));
 const Faq = lazy(() => import("./components/Faq"));
@@ -22,7 +22,6 @@ function App() {
         <AboutUs />
         <Domains />
         <Timeline />
-        <Judges />
         <PrizePool />
         <Sponsors />
         <Faq />
